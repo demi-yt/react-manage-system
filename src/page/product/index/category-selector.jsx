9840b3edf27e7199912153c5c69f834088ae8d5d
demi <demi_yu@patazon.net>
2019-01-18 17:53:16 +0800

    '新增商品'

---
+++ src/page/product/index/category-selector.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MUtil from '../../../util/index.jsx';
+import Product from '../../../service/product-service.jsx';
+
+const _index = new MUtil();
+const _product = new Product();
+
+import './category-selector.scss';
---
+++ src/page/product/index/save.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import MUtil from '../../../util/index.jsx';
+import Product from '../../../service/product-service.jsx';
+import PageTitle from '../../../component/page-title/index.jsx'
